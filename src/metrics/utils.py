fa4d170c1893ee9f6fe431bd96d546ff7a7e1d84
NickolayYakovchukfm <creepertrase@gmail.com>
2025-10-11 21:22:47 +0300

    Add cer and wer calculations with ruff

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,13 +1,36 @@
 # Based on seminar materials
+import editdistance
 
 # Don't forget to support cases when target_text == ''
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    """
+    Input:
+    target_text (str)
+    predicted_text (str)
+
+    Output:
+    cer (1, )
+    """
+    assert (
+        len(target_text) != 0
+    ), f"!!!ALERT: watch this on calc_cer: target_text_len = {len(target_text)}"
+    return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    """
+    Input:
+    target_text (str)
+    predicted_text (str)
+
+    Output:
+    wer (1, )
+    """
+    assert (
+        len(target_text) != 0
+    ), f"!!!ALERT: watch this on calc_wer : target_text_len = {len(target_text)}"
+    return editdistance.eval(target_text.split(), predicted_text.split()) / len(
+        target_text.split()
+    )
